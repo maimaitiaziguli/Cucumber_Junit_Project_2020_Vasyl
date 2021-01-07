57ee0c36deddfa1285d9b9be9237c8490aaca234
maimaitiaziguli <arzuyum507@gmail.com>
2021-01-06 21:13:55 -0800

    Browser Util added

--- src/test/java/com/vytrack/Step_definitions/CreateCarStepDefinitions.java
+++ src/test/java/com/vytrack/Step_definitions/CreateCarStepDefinitions.java
@@ -1,17 +1,22 @@
 package com.vytrack.Step_definitions;
 
+import com.vytrack.pages.CreateCarPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
+import java.util.Map;
+
 public class CreateCarStepDefinitions {
+
+    CreateCarPage createCarPage = new CreateCarPage();
 
     @Given("User clicks on create car button")
     public void user_clicks_on_create_car_button() {
-
+createCarPage.clickOnCreateCar();
 
     }
     @When("User adds new vehicles information")
-    public void user_adds_new_vehicles_information(io.cucumber.datatable.DataTable dataTable) {
+    public void user_adds_new_vehicles_information(Map<String, String> dataTable) {
 
 
     }
--- src/test/java/com/vytrack/pages/CreateCarPage.java
+++ src/test/java/com/vytrack/pages/CreateCarPage.java
@@ -1,11 +1,26 @@
 package com.vytrack.pages;
 
+import com.vytrack.utils.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CreateCarPage extends BasePage{
 
     @FindBy(css ="[title='Create Car']")
     private WebElement createCarBtn;
+
+    public void clickOnCreateCar(){
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.elementToBeClickable(createCarBtn));
+        System.out.println("Clicking on 'Create car button'");
+
+
+    }
+
+
 
 }
